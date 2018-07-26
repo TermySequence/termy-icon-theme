151f492440d557ae9e0888a5c7a811ece14eb836
Eamon Walsh <ewalsh@termysequence.com>
2018-07-26 16:56:38 -0400

    Drop custom dist logic from mkdist script

--- mkdist.sh
+++ mkdist.sh
@@ -5,26 +5,10 @@ package=$(awk '/^PackageName:/ {print $2}' LICENSE.spdx)
 version=$(awk '/^PackageVersion:/ {print $2}' LICENSE.spdx)
 name="${package}-${version}"
 tarfile=/tmp/${name}.tar
-tmpdir=/tmp/${name}
-
-rm -rf ${tmpdir}
-mkdir -p ${tmpdir} || exit 2
 
 git archive --prefix=${name}/ -o ${tarfile} ${commit} || exit 3
-
-for theme in default; do
-    icondir=${tmpdir}/icons/${theme}
-    mkdir -p ${icondir} || exit 2
-    svgdir=${tmpdir}/svg/${theme}
-    mkdir -p ${svgdir} || exit 2
-    (cd icons/${theme} && source dist.sh ${icondir} ${svgdir}) || exit 4
-done
-
-tar -uf ${tarfile} -C /tmp ${name} || exit 5
-
 xz <${tarfile} >${name}.tar.xz || exit 6
 
 rm ${tarfile}
-rm -r ${tmpdir}
 
 echo Success
